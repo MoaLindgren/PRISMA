27accc8d9465db7626b1ebe3b68c9d42ea5590f2
MoaLindgren <moa.lindgren@outlook.com>
2018-12-04 22:14:40 +0100

    Imported assets + Movement Script Added

---
+++ PRISMA/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    float moveSpeed;
+    float moveHorizontal, moveVertical;
+
+    Quaternion rot;
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(movement);
+        }
+    }
+}
