8a6e958fcb844e71bccc122cd31bd64d6d1478c5
MoaLindgren <moa.lindgren@outlook.com>
2018-12-05 13:25:02 +0100

    Open/Close backpack
    +UI Canvas
    +UI buttons, image
    +MenuManager Script

---
+++ PRISMA/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject popUpImage;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (popUpImage.active)
+            {
+                CloseBackpack();
+            }
+            else
+            {
+                OpenBackpack();
+            }
+        }
+    }
+    public void OpenBackpack()
+    {
+        popUpImage.SetActive(true);
+    }
+    public void CloseBackpack()
+    {
+        popUpImage.SetActive(false);
+    }
+}
