58d1110e0e5ca877491e2b32b62b95305c6c0e98
MoaLindgren <moa.lindgren@outlook.com>
2018-12-06 12:26:14 +0100

    Camera Script added
    Camera now fixed to player

---
+++ PRISMA/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    float yPos, zPos;
+
+    void Update()
+    {
+        Vector3 pos = player.transform.position;
+        pos.y += yPos;
+        pos.z += zPos;
+        transform.position = pos;
+    }
+}
