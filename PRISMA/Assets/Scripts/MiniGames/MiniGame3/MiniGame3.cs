ca63daa1ecdcd7d622aa8596917460c807819117
MoaLindgren <moa.lindgren@outlook.com>
2019-01-03 10:34:09 +0100

    Minigame3 update
    tulkort-plants only spawn at minigame3 now

--- PRISMA/Assets/Scripts/MiniGames/MiniGame3/MiniGame3.cs
+++ PRISMA/Assets/Scripts/MiniGames/MiniGame3/MiniGame3.cs
@@ -6,30 +6,54 @@ public class MiniGame3 : MonoBehaviour
 {
     [SerializeField]
     GameObject tulkortPrefab, plane;
+    [SerializeField]
+    float clickTimer;
+    float counter;
     Vector3 clickPosition;
     bool gameStart;
+    [SerializeField]
+    bool spawnReady;
 
     void Start()
     {
+        spawnReady = true;
+        counter = clickTimer;
         gameStart = true;
     }
 
     void Update()
     {
+        if(!spawnReady)
+        {
+            counter -= Time.deltaTime;
+            if (counter <= 0)
+            {
+                spawnReady = true;
+            }
+        }
+
         if(gameStart)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0) && spawnReady)
             {
+                counter = clickTimer;
+                spawnReady = false;
+
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hit;
 
-                if (Physics.Raycast(ray, out hit/*, Mathf.Infinity*/))
+                if (Physics.Raycast(ray, out hit))
                 {
                     clickPosition = hit.point;
                     Instantiate(tulkortPrefab, clickPosition, Quaternion.identity);
                 }
             }
         }
+
+    }
+
+    void SpawnBeatles()
+    {
 
     }
 
