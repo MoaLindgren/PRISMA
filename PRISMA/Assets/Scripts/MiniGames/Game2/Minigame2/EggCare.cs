382305c3eb112e8605d3cf050ac592fbd9d3b3fb
lucepuce <lucas_keno@hotmail.com>
2019-01-08 15:59:24 +0100

    Skapat ett game 2 och byggt ett ruva ägg spel

---
+++ PRISMA/Assets/Scripts/MiniGames/Game2/Minigame2/EggCare.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggCare : MonoBehaviour
+{
+
+    [SerializeField]
+    float timer, spawnFlowerTimer, lowSpawnRate, highSpawnRate;
+    float spawnCounter, rnd, counter;
+
+    [SerializeField]
+    int numberOfWeed, maxDeathCount;
+    public int deadEggs;
+
+    [SerializeField]
+    GameObject weed;
+    GameObject player;
+
+    GameObject[] eggLocations;
+
+
+    public List<GameObject> takenLocation;
+
+    PlayerBehaviour playerBehaviour;
+    testGameManager testGame;
+    bool gameStart;
+
+    void Start()
+    {
+        takenLocation = new List<GameObject>();
+        eggLocations = GameObject.FindGameObjectsWithTag("Egg");
+        counter = timer;
+        deadEggs = 0;
+        gameStart = true;
+
+        foreach (GameObject egg in eggLocations)
+        {
+            egg.GetComponent<Eggs>().enabled = true;
+        }
+
+        player = GameObject.FindGameObjectWithTag("player");
+        playerBehaviour = player.GetComponent<PlayerBehaviour>();
+        testGame = GetComponent<testGameManager>();
+        Randomize();
+    }
+    void Update()
+    {
+        if (gameStart)
+        {
+            if (counter > 0)
+                counter -= Time.deltaTime;
+
+            else if (deadEggs < maxDeathCount && counter <= 0)
+            {
+                Win();
+            }
+            if (deadEggs >= maxDeathCount)
+            {
+                gameStart = false;
+                testGame.EndGame(false);
+                foreach (GameObject egg in eggLocations)
+                {
+                    egg.GetComponent<Eggs>().enabled = false;
+                }
+
+            }
+        }
+
+
+    }
+
+    void Win()
+    {
+        gameStart = false;
+        testGame.EndGame(true);
+        foreach (GameObject egg in eggLocations)
+        {
+            egg.GetComponent<Eggs>().enabled = false;
+        }
+    }
+
+
+    public void DeadFlower()
+    {
+        deadEggs++;
+    }
+
+    public void LevelFlower(int level)
+    {
+        if (level == 2)
+        {
+            //print("nu är jag lvl 2");
+        }
+
+        else if (level == 3)
+        {
+            //print("nu är jag lvl 3");
+        }
+    }
+    void Randomize()
+    {
+        rnd = Random.Range(lowSpawnRate, highSpawnRate);
+    }
+}
---
+++ PRISMA/Assets/Scripts/MiniGames/Game2/Minigame2/Eggs.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eggs : MonoBehaviour {
+
+    [SerializeField]
+    float minTime, maxTime;
+
+    [SerializeField]
+    int eggLevel;
+    [SerializeField]
+    float growTimer, growTimerStart;
+
+    //public GameObject myLocation;
+
+    EggCare eggCare;
+
+    void Start()//Slumpa ett värde i början följt av att sätta alla andra värden
+    {
+        eggCare = GameObject.Find("Station 2").GetComponent<EggCare>();
+    }
+    void Update()//Ticka ner tiden det tar för plantor att växa
+    {
+        growTimer -= Time.deltaTime;
+        if (growTimer < 0)
+            GrowWeed();
+    }
+
+    public void StartGame()
+    {
+        RandomizeValue();
+        growTimer = growTimerStart;
+        eggLevel = 0;
+    }
+
+    void GrowWeed()
+    {
+        growTimer = growTimerStart;
+        eggLevel++;
+        IncreaseSize();
+        EatPlant();
+        RandomizeValue();
+    }
+
+    void RandomizeValue()//Slumpa ett värde för hur ofta plantorna ska växa
+    {
+        float rndTime = Random.Range(minTime, maxTime);
+        growTimerStart = rndTime;
+    }
+
+    private void OnMouseDown()//När spelaren klickar på ogräset så förstörs det
+    {
+
+        eggLevel = 0;
+
+    }
+
+    void EatPlant()
+    {
+        if (eggLevel == 8)
+        {
+
+            eggCare.DeadFlower();
+            print("nu dog ett ägg");
+        }
+    }
+
+    void IncreaseSize()//Här ska vi ha visuel feedback som visar på att ägget blir kallare
+    {
+        if (eggLevel == 2)
+
+        {
+            eggCare.LevelFlower(2);
+        }
+
+        if (eggLevel == 3)
+        {
+            eggCare.LevelFlower(3);
+        }
+    }
+}
