dade772ba9cc37963b532732cc84b9fb6965719e
MoaLindgren <moa.lindgren@outlook.com>
2019-01-09 15:42:25 +0100

    Minigame 1 - 3 working

--- PRISMA/Assets/Scripts/MiniGames/Game1/MiniGame3/MiniGame3.cs
+++ PRISMA/Assets/Scripts/MiniGames/Game1/MiniGame3/MiniGame3.cs
@@ -8,95 +8,111 @@ public class MiniGame3 : MonoBehaviour
     GameObject tulkortPrefab, waypointsParent, player;
     List<GameObject> waypoints;
     GameObject destination;
-    [SerializeField]
-    float clickTimer, max;
-    float clickTimerCounter, bugTimerCounter, rndTimer;
     Vector3 clickPosition;
-    bool gameStart;
-    [SerializeField]
-    bool spawnReady;
-    float speed;
+
     PlayerBehaviour playerBehaviour;
     testGameManager gameManager;
-    int destinationIndex;
-    bool move;
 
+    [SerializeField]
+    float clickTimer, max;
+    float clickTimerCounter, bugTimerCounter, rndTimer, speed;
+    [SerializeField]
+    int score, numberOfPlants;
+    int destinationIndex, plantCounter;
+    bool gameStart, move, plantReady;
 
     void Start()
     {
         gameManager = gameObject.GetComponent<testGameManager>();
-        destinationIndex = 0;
-        move = true;
         playerBehaviour = player.GetComponent<PlayerBehaviour>();
+
+        InitializeGame();
+        RandomizeValues();
+
+        move = true;
+        plantReady = true;
+        gameStart = true;
+    }
+    void InitializeGame()
+    {
         speed = playerBehaviour.moveSpeed;
+        clickTimerCounter = clickTimer;
+
+        plantCounter = 0;
+        destinationIndex = 0;
         waypoints = new List<GameObject>();
-        for(int i = 0; i < waypointsParent.transform.childCount; i++)
+        for (int i = 0; i < waypointsParent.transform.childCount; i++)
         {
             waypoints.Add(waypointsParent.transform.GetChild(i).gameObject);
         }
-        playerBehaviour.moveable = false;
-        spawnReady = true;
-        clickTimerCounter = clickTimer;
-        RandomizeValues();
-        gameStart = true;
     }
-
     void Update()
     {
-        destination = waypoints[destinationIndex];
         if (gameStart)
         {
-            if (!spawnReady)
+            if (!plantReady)
             {
                 clickTimerCounter -= Time.deltaTime;
                 if (clickTimerCounter <= 0)
                 {
-                    spawnReady = true;
+                    plantReady = true;
                 }
             }
-            if (Input.GetMouseButtonDown(0) && spawnReady)
+            if (Input.GetMouseButtonDown(0) && plantReady)
             {
-                clickTimerCounter = clickTimer;
-                spawnReady = false;
-
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-
-                if (Physics.Raycast(ray, out hit))
-                {
-                    clickPosition = hit.point;
-                    Instantiate(tulkortPrefab, clickPosition, Quaternion.identity);
-                }
+                SpawnPlants();
             }
             bugTimerCounter -= Time.deltaTime;
-            if(bugTimerCounter <= 0)
-            {
 
-            }
-            if(move)
-            {
-                float step = 0.1f;
-                player.transform.position = Vector3.MoveTowards(player.transform.position, destination.transform.position, step);
-                player.transform.LookAt(destination.transform.position);
-
-
-                if (Vector3.Distance(destination.transform.position, player.transform.position) <= 2)
-                {
-                    destinationIndex++;
-                    if(destinationIndex == waypoints.Count)
-                    {
-                        move = false;
-                        gameManager.EndGame(true);
-                        gameStart = false;
-                    }
-                    return;
-                }
-            }
+            PlayerAutomaticMovement();
         }
     }
 
+    void SpawnPlants()
+    {
+        clickTimerCounter = clickTimer;
+        plantReady = false;
 
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject clickObject = hit.transform.gameObject;
+            clickPosition = hit.point;
+            if (plantCounter < numberOfPlants)
+            {
+                if (clickObject.tag == "Bugs")
+                {
+                    score++;
+                    Instantiate(tulkortPrefab, clickObject.transform.position, Quaternion.identity);
+                }
+                else if (clickObject.name == "Terrain")
+                {
+                    Instantiate(tulkortPrefab, clickPosition, Quaternion.identity);
+                }
+                plantCounter++;
+            }
+        }
+    }
+    void PlayerAutomaticMovement()
+    {
+        destination = waypoints[destinationIndex];
+        float step = 0.1f;
+        player.transform.position = Vector3.MoveTowards(player.transform.position, destination.transform.position, step);
+        player.transform.LookAt(destination.transform.position);
+
+        if (Vector3.Distance(destination.transform.position, player.transform.position) <= 2)
+        {
+            destinationIndex++;
+            if (destinationIndex == waypoints.Count)
+            {
+                gameManager.EndGame(true);
+                gameStart = false;
+            }
+            return;
+        }
+    }
     void RandomizeValues()
     {
         rndTimer = Random.Range(1, max);
