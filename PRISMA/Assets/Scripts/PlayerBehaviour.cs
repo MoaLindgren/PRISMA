24ebb0aa21f41965367092d01dd2553835329533
MoaLindgren <moa.lindgren@outlook.com>
2019-01-22 19:15:42 +0100

    updated player behaviour

--- PRISMA/Assets/Scripts/PlayerBehaviour.cs
+++ PRISMA/Assets/Scripts/PlayerBehaviour.cs
@@ -7,17 +7,14 @@ public class PlayerBehaviour : MonoBehaviour
     public float moveSpeed;
     [SerializeField]
     float rotationSpeed, rotationUp, rotationDown;
-    float moveHorizontal, moveVertical;
-    Vector3 destination, movement;
+    //float moveHorizontal, moveVertical;
+    //Vector3 movement;
     public bool moveable;
-    Quaternion rot;
-    //Animator anim;
-    GameObject camera;
+    GameObject mainCamera;
 
     void Start()
     {
-        camera = GameObject.Find("Main Camera");
-        //anim = GetComponent<Animator>();
+        mainCamera = GameObject.Find("Main Camera");
         moveable = true;
     }
     void Update()
@@ -29,56 +26,39 @@ public class PlayerBehaviour : MonoBehaviour
     }
     void Move()
     {
-        moveVertical = Input.GetAxis("Vertical");
-        moveHorizontal = Input.GetAxis("Horizontal");
-        movement = new Vector3(-moveVertical, 0, moveHorizontal);
+        //moveVertical = Input.GetAxis("Vertical");
+        //moveHorizontal = Input.GetAxis("Horizontal");
+        //movement = new Vector3(-moveVertical, 0, moveHorizontal);
 
 
         if (!Input.GetKey(KeyCode.LeftControl))
         {
             transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * rotationSpeed);
         }
+        else if(Input.GetKey(KeyCode.LeftControl))
+        {
+            mainCamera.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * rotationSpeed);
 
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            mainCamera.transform.rotation = transform.rotation;
+        }
 
-        print(transform.rotation.x);
-
-
-        //transform.Rotate(-Vector3.down * rotationSpeed * Time.deltaTime);
-
-
-        //if (movement != Vector3.zero)
-        //{
-
-        //    transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
-        //}
         transform.position = new Vector3(transform.position.x, 10, transform.position.z);
-        ////Back and turn left:
-        //if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S))
-        //{
-        //    transform.Rotate(-Vector3.down * rotationSpeed * Time.deltaTime);
-        //}
-        //Turn left:
+
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
         }
-        ////Back and turn right:
-        //if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))
-        //{
-        //    transform.Rotate(-Vector3.up * rotationSpeed * Time.deltaTime);
-        //}
-        //Turn right:
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
         }
-        //Forward:
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-            //transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
         }
-        //Back:
         else if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
