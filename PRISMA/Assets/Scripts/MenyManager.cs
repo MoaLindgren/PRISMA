d6959afe5c96bf6223a4e0fe7418fea200dd5dba
lucepuce <lucas_keno@hotmail.com>
2019-02-08 13:11:55 +0100

    MenyManager + vi har en meny

---
+++ PRISMA/Assets/Scripts/MenyManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class MenyManager : MonoBehaviour {
+
+    public void StartGame1()
+    {
+        SceneManager.LoadScene("Game3");
+    }
+
+    public void StartGame2()
+    {
+        SceneManager.LoadScene("Game4");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
