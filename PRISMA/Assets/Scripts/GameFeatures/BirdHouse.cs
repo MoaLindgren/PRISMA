4ea7d410bd44933ce1a2b70ccd9b4969380d9d1c
lucepuce <lucas_keno@hotmail.com>
2019-02-08 12:08:40 +0100

    Man kan sätta ut fågelholkar genom att klicka på specifika träd

---
+++ PRISMA/Assets/Scripts/GameFeatures/BirdHouse.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdHouse : MonoBehaviour {
+
+
+    [SerializeField]
+    GameObject birdHouse;
+
+    Transform birdHouseLocation;
+
+    bool hasBirdHouse;
+
+    void Start()
+    {
+        hasBirdHouse = false;
+        birdHouseLocation.position = gameObject.transform.position;
+    }
+
+    void OnMouseDown()
+    {
+        //om du har rätt item klickan
+
+        //Lås upp rätt achievemnt + starta texten
+
+        if(hasBirdHouse == false)
+        {
+            Instantiate(birdHouse, gameObject.transform);
+            hasBirdHouse = true;
+        }
+        
+    }
+}
