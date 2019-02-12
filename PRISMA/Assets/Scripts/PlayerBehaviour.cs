356fe977eda1e316ff626f499683271e8b660a35
MoaLindgren <moa.lindgren@outlook.com>
2019-02-12 15:26:11 +0100

    update platermovement

--- PRISMA/Assets/Scripts/PlayerBehaviour.cs
+++ PRISMA/Assets/Scripts/PlayerBehaviour.cs
@@ -26,44 +26,38 @@ public class PlayerBehaviour : MonoBehaviour
     }
     void Move()
     {
-        //float vertical = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        //float horizontal = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        //float xRotation = Input.GetAxis("Mouse X");
 
-        //Vector3 movement = new Vector3(-horizontal, 10, -vertical);
-        //Vector3 rotation = new Vector3(0, xRotation, 0);
-        //Quaternion rot = Quaternion.Euler(rotation * rotationSpeed * Time.deltaTime);
-        //Rigidbody rb = GetComponent<Rigidbody>();
-
-        //if(movement != Vector3.zero)
-        //{
-        //    rb.MovePosition(transform.position + movement);
-        //    rb.MoveRotation(transform.rotation * rot);
-        //}
-
+        Rigidbody rb = GetComponent<Rigidbody>();
 
         if (!Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.LeftShift))
         {
             transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * rotationSpeed);
         }
 
-        transform.position = new Vector3(transform.position.x, 10, transform.position.z);
+        if(transform.position != Vector3.zero)
+        {
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x, 10, transform.position.z);
+            }
         }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
-        }
+        
     }
 }
